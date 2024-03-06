8ff7c45d3ec3c3aaaea70b07bdc5130e07eba252
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2024-03-06 15:44:00 +0100

    Add project structure

---
+++ Our.Umbraco.KeyVault.Core/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Our.Umbraco.KeyVault.Core.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static T GetConfiguredInstance<T>(this IConfiguration configuration, string sectionName) where T : new()
+    {
+        var instance = new T();
+
+        var section = configuration.GetSection(sectionName);
+        section.Bind(instance);
+
+        return instance;
+    }
+}
---
+++ Our.Umbraco.KeyVault.Core/PackageConstants-Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace Our.Umbraco.KeyVault.Core;
+
+public partial class PackageConstants
+{
+    public static class Configuration
+    {
+        public static class SettingsSections
+        {
+            public const string KeyVault = nameof(KeyVault);
+        }
+    }
+}
